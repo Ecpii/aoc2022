99a78c8b535962b196dcb9be1ba1cc9f571ba2ad
Ecpi <28408948+Ecpii@users.noreply.github.com>
2022-12-04 00:10:18 -0500

    finish day 4

--- day04/second.py
+++ day04/second.py
@@ -6,7 +6,7 @@ for line in lines:
     groups = line.split(",")
     low1, hi1 = map(int, groups[0].split("-"))
     low2, hi2 = map(int, groups[1].split("-"))
-    if (low1 <= low2 and hi1 >= hi2) or (low1 >= low2 and hi1 <= hi2):
+    if (low2 <= low1 <= hi2) or (low2 <= hi1 <= hi2) or (low1 <= low2 <= hi1) or (low1 <= hi2 <= hi1):
         num_sup_sets += 1
 
 print(num_sup_sets)
