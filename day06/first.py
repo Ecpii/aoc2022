aca629b37a5c7aad681eec36679c947b7e5f102b
Ecpi <28408948+Ecpii@users.noreply.github.com>
2022-12-06 04:13:01 -0500

    finish day 6

--- day06/first.py
+++ day06/first.py
@@ -1,1 +1,12 @@
 with open("input.txt") as inp:
+    line = inp.read()[:-1]
+
+l, r = 0, 14
+window = set(line[l:r])
+
+while len(window) != 14:
+    l += 1
+    r += 1
+    window = set(line[l:r])
+
+print(r)
